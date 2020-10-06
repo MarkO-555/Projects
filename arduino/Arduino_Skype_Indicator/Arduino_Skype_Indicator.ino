473b0e5589c1e6aed908f5721e9d7b90de49dcf3
JHstrutton2000 <49569114+JHstrutton2000@users.noreply.github.com>
2020-10-06 14:31:00 -0700

    Update Arduino_Skype_Indicator.ino

--- arduino/Arduino_Skype_Indicator/Arduino_Skype_Indicator.ino
+++ arduino/Arduino_Skype_Indicator/Arduino_Skype_Indicator.ino
@@ -31,28 +31,20 @@ void loop() {
 
     // look for the next valid integer in the incoming serial stream:
     int green = Serial.parseInt();
-    // do it again:
     int yellow = Serial.parseInt();
-    // do it again:
     int red = Serial.parseInt();
 
     // look for the newline. That's the end of your
     // sentence:
     if (Serial.read() == '\n') {
       // constrain the values to 0 - 255
-      green = constrain(green + yellow, 0, 255);
-      red = constrain(red + yellow, 0, 255);
-//      yellow = constrain(yellow, 0, 255);
+      green = constrain(green+yellow, 0, 255);
+      red = constrain(red+yellow, 0, 255);
       
       // fade the green, yellow, and red LEDs:
       analogWrite(greenPin, green);
+      
       analogWrite(redPin, red);
-//      analogWrite(yellowPin, yellow);
-
-      // print the three numbers in one string as hexadecimal:
-      Serial.print(green, HEX);
-//      Serial.print(yellow, HEX);
-      Serial.println(red, HEX);
     }
   }
 }
